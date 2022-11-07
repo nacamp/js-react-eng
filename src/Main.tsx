b0796117ea0e5a1fe62b88e9d4e5535a7e286bde
jimmy <najimmy@gmail.com>
2022-11-07 18:41:24 +0900

    [feature]스크립트,공부한내용을 로컬스토리지에서 가져오게 설정

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,7 +1,42 @@
 import React, { useEffect, useState, useRef } from 'react';
-import { Box, Button, TextField, Paper, Typography, Chip, Avatar, useMediaQuery } from '@mui/material';
+import {
+  Box,
+  Button,
+  TextField,
+  Paper,
+  Typography,
+  Chip,
+  Avatar,
+  useMediaQuery,
+  Tab,
+  Tabs,
+  Card,
+  CardHeader,
+  CardContent,
+  CardActions,
+} from '@mui/material';
 import { useTheme } from '@mui/material/styles';
 import copy from 'copy-to-clipboard';
+
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
+}
+
+function TabPanel(props: TabPanelProps) {
+  const { children, value, index } = props;
+
+  return (
+    <>
+      {value === index && (
+        <Box>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </>
+  );
+}
 
 function Question({ idx, kr, en, onChange }: any) {
   const theme = useTheme();
@@ -130,9 +165,11 @@ function Question({ idx, kr, en, onChange }: any) {
 
 function Main() {
   const scrollBoxRef = useRef<HTMLUListElement>();
+  const [scriptName, setScriptName] = useState<string>('');
   const [data, setData] = useState<any>([]);
-  const getData = () => {
-    fetch('data.json', {
+  const getData = (name: string) => {
+    console.log(`xxxxscript/${name}.json`);
+    fetch(`script/${name}.json`, {
       headers: {
         'Content-Type': 'application/json',
         Accept: 'application/json',
@@ -148,9 +185,18 @@ function Main() {
 
   // 내가 공부한 자료
   const [studiedData, setStudiedData] = useState<any>({});
+
+  function loadData(){
+    const _scriptName:string|null = localStorage.getItem('scriptName');
+    if(_scriptName){
+      setScriptName(_scriptName);
+      getData(_scriptName);
+      setStudiedData(JSON.parse(localStorage.getItem(`${_scriptName}-data`) || '{}'));
+    }
+  }
+
   useEffect(() => {
-    getData();
-    setStudiedData(JSON.parse(localStorage.getItem('eng') || '{}'));
+    loadData();
   }, []);
 
   const [text, setText] = useState('');
@@ -197,28 +243,102 @@ function Main() {
     } catch {}
   }, [studiedData]);
 
+  const [tabId, setTabId] = React.useState(0);
+  const handleTabChange = (event: React.SyntheticEvent, newValue: number) => {
+    setTabId(newValue);
+  };
+
+  const [rawStudiedData, setRawStudiedData] = useState<string>('');
+  const handleSave = (type:string) => {
+    if(type ==='ScriptName'){
+      localStorage.setItem('scriptName', scriptName);
+    } else if(type ==='StudiedData'){
+      localStorage.setItem(`${scriptName}-data`, rawStudiedData);
+    }
+    loadData();
+  };
+
   return (
     <Box sx={{ p: 2 }}>
-      <Paper elevation={3} sx={{ pl: 1 }}>
-        <Box
-          ref={scrollBoxRef}
-          style={{
-            display: 'block',
-            width: '100%',
-            overflow: 'auto',
-            height: '500px',
-          }}
-        >
-          {prevData &&
-            prevData.length > 0 &&
-            prevData.map((item: any, i: number) => (
-              <Typography key={i} variant="body2" gutterBottom>
-                {item.idx}: {item.en}-{item.kr} <br />
-                My answer : {studiedData[i]}
-              </Typography>
-            ))}
-        </Box>
-      </Paper>
+      <Tabs sx={{ m: 1 }} value={tabId} onChange={handleTabChange}>
+        <Tab label="Script" sx={{ textTransform: 'none' }} />
+        <Tab label="Setting" sx={{ textTransform: 'none' }} />
+      </Tabs>
+      <TabPanel value={tabId} index={0}>
+        <Paper elevation={3} sx={{ pl: 1 }}>
+          <Box
+            ref={scrollBoxRef}
+            style={{
+              display: 'block',
+              width: '100%',
+              overflow: 'auto',
+              height: '500px',
+            }}
+          >
+            {prevData &&
+              prevData.length > 0 &&
+              prevData.map((item: any, i: number) => (
+                <Typography key={i} variant="body2" gutterBottom>
+                  {item.idx}: {item.en}-{item.kr} <br />
+                  My answer : {studiedData[i]}
+                </Typography>
+              ))}
+          </Box>
+        </Paper>
+      </TabPanel>
+      <TabPanel value={tabId} index={1}>
+        <Card elevation={3}>
+          {/* <CardHeader title="." /> */}
+          <CardContent>
+            {/* <Typography variant="body1" component="p">
+              Please enter something. <br />
+            </Typography>
+            <TextField label="content" /> */}
+            <Box
+              sx={{
+                display: 'flex',
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                mt: 2,
+                mx: 2,
+              }}
+            >
+              <TextField
+                label="스크립트 이름"
+                value={scriptName}
+                variant="standard"
+                sx={{ mr: 2 }}
+                onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                  setScriptName(event.target.value);
+                }}
+              />
+              <Button onClick={(e) => handleSave('ScriptName')}>이름저장</Button>
+            </Box>
+            <Box
+              sx={{
+                display: 'flex',
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                mt: 2,
+                mx: 2,
+              }}
+            >
+              <TextField
+                multiline
+                fullWidth
+                label="스터디 내용"
+                variant="standard"
+                value={rawStudiedData}
+                sx={{ mr: 2}}
+                onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                  setRawStudiedData(event.target.value);
+                }}
+              />
+              <Button sx={{flexBasis:'72px'}} onClick={(e) => handleSave('StudiedData')}>내용저장</Button>
+            </Box>
+          </CardContent>
+        </Card>
+      </TabPanel>
 
       {/* current line, 완료된 라인 */}
       <Box
