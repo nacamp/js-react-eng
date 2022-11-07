9ca45ad568e574c321a0afdc159a14ed82f8aea9
jimmy <najimmy@gmail.com>
2022-11-07 13:56:46 +0900

    [refactor]의미있는 이름으로변경,불필요한로그제거, 코드위치변경,에러유발코드수정

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, useRef } from 'react';
-import { Box, Button, TextField, Paper, Typography, Chip, Avatar, useMediaQuery, Divider } from '@mui/material';
+import { Box, Button, TextField, Paper, Typography, Chip, Avatar, useMediaQuery } from '@mui/material';
 import { useTheme } from '@mui/material/styles';
 import copy from 'copy-to-clipboard';
 
@@ -12,10 +12,12 @@ function Question({ idx, kr, en, onChange }: any) {
   const [hint, setHint] = useState<string>('');
 
   useEffect(() => {
-    setHintStep(0);
-    setText('');
-    setHints(en.split(' '));
-    setHint('');
+    if (en) {
+      setHintStep(0);
+      setText('');
+      setHints(en.split(' '));
+      setHint('');
+    }
   }, [en]);
 
   function handleChange(text: string) {
@@ -100,7 +102,6 @@ function Question({ idx, kr, en, onChange }: any) {
             handleChange(event.target.value);
           }}
           sx={isMobile ? { mr: 5 } : { mr: 2 }}
-          // sx={{ ml: 2 }}
           required
         />
         <Box
@@ -108,8 +109,6 @@ function Question({ idx, kr, en, onChange }: any) {
             display: 'flex',
             flexDirection: 'row',
             justifyContent: 'space-between',
-            // p: 1,
-            // m: 1,
             flexBasis: '100%',
             verticalAlign: 'bottom',
           }}
@@ -130,7 +129,7 @@ function Question({ idx, kr, en, onChange }: any) {
 }
 
 function Main() {
-  const messageBoxRef = useRef<HTMLUListElement>();
+  const scrollBoxRef = useRef<HTMLUListElement>();
   const [data, setData] = useState<any>([]);
   const getData = () => {
     fetch('data.json', {
@@ -140,30 +139,27 @@ function Main() {
       },
     })
       .then(function (response) {
-        //console.log(response);
         return response.json();
       })
       .then(function (myJson) {
-        //console.log(myJson);
         setData(myJson);
       });
   };
+
+  // 내가 공부한 자료
   const [studiedData, setStudiedData] = useState<any>({});
   useEffect(() => {
     getData();
-
     setStudiedData(JSON.parse(localStorage.getItem('eng') || '{}'));
   }, []);
 
-  const [line, setLine] = useState<number>(0);
-  const [prevData, setPrevData] = useState<any>([]);
-
   const [text, setText] = useState('');
-
   function handleChange(text: string) {
     setText(text);
   }
 
+  const [line, setLine] = useState<number>(0);
+  const [prevData, setPrevData] = useState<any>([]);
   function handleNext() {
     const nextLine = line + 1;
     setLine(nextLine);
@@ -172,42 +168,32 @@ function Main() {
       return;
     }
 
-    // console.log(data.slice(0,line+1))
     const answer: { [key: number]: string } = {};
     answer[line] = text;
     const answerList = { ...studiedData, ...answer };
     setStudiedData(answerList);
-    // console.log(studiedData, { nextLine: nextLine});
     localStorage.setItem('eng', JSON.stringify(answerList));
-    // JSON.parse(localStorage.getItem('nums'))
     setText('');
-    handleStudiedLine();
   }
 
   function handleCopy() {
-    // console.log(JSON.parse(localStorage.getItem('eng')|| '{}'))
-    // console.log( localStorage.getItem('eng')|| '{}')
     copy(localStorage.getItem('eng') || '{}');
   }
 
-  const [studiedLine, setStudiedLine] = useState<number>(0);
-  function handleStudiedLine() {
-    // console.log(studiedData);
-    // setStudiedLine(+Object.keys(studiedData).reduce((a, b) => (+a > +b ? a : b)));
-  }
-
   const scrollToBottom = () => {
-    if (messageBoxRef.current) {
-      messageBoxRef.current.scrollTop = messageBoxRef.current.scrollHeight;
+    if (scrollBoxRef.current) {
+      scrollBoxRef.current.scrollTop = scrollBoxRef.current.scrollHeight;
     }
   };
   useEffect(() => {
     scrollToBottom();
   }, [prevData]);
 
+  // 공부했던 가장 마지막 라인
+  const [studiedMaxLine, setStudiedMaxLine] = useState<number>(0);
   useEffect(() => {
     try {
-      setStudiedLine(+Object.keys(studiedData).reduce((a, b) => (+a > +b ? a : b)));
+      setStudiedMaxLine(+Object.keys(studiedData).reduce((a, b) => (+a > +b ? a : b)));
     } catch {}
   }, [studiedData]);
 
@@ -215,7 +201,7 @@ function Main() {
     <Box sx={{ p: 2 }}>
       <Paper elevation={3} sx={{ pl: 1 }}>
         <Box
-          ref={messageBoxRef}
+          ref={scrollBoxRef}
           style={{
             display: 'block',
             width: '100%',
@@ -223,19 +209,14 @@ function Main() {
             height: '500px',
           }}
         >
-          <Typography variant="body2" gutterBottom>
-            {prevData &&
-              prevData.length > 0 &&
-              prevData.map((item: any, i: number) => (
-                <>
-                  <p>
-                    {' '}
-                    {item.idx}: {item.en}-{item.kr}
-                  </p>
-                  <p> My answer : {studiedData[i]}</p>
-                </>
-              ))}
-          </Typography>
+          {prevData &&
+            prevData.length > 0 &&
+            prevData.map((item: any, i: number) => (
+              <Typography key={i} variant="body2" gutterBottom>
+                {item.idx}: {item.en}-{item.kr} <br />
+                My answer : {studiedData[i]}
+              </Typography>
+            ))}
         </Box>
       </Paper>
 
@@ -255,13 +236,11 @@ function Main() {
           variant="standard"
           sx={{ mr: 2 }}
           onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-            setLine(+event.target.value);
             setPrevData(data.slice(0, +event.target.value));
-            handleStudiedLine();
-            //handleLine(event.target.value);
+            setLine(+event.target.value);
           }}
         />
-        <Chip avatar={<Avatar>{studiedLine}</Avatar>} label="studied" onClick={(e) => handleStudiedLine()} />
+        <Chip avatar={<Avatar>{studiedMaxLine}</Avatar>} label="studied" />
         <Chip label="copy" onClick={(e) => handleCopy()} />
         <Chip label="next" onClick={(e) => handleNext()} />
       </Box>
